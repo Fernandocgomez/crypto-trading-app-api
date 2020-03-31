c133882ebd5d5040e428ef4d3ca704d9bbc9f821
fernandocgomez <fernandocgomez@live.com>
2020-03-31 01:28:25 -0500

    auth: render portafolio

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -6,7 +6,7 @@ class AuthController < ApplicationController
     user = User.find_by(username: params[:username])
 
     if user && user.authenticate(params[:password])
-      render json: {user: user, token: encode_token({user_id: user.id})}
+      render json: {user: user, token: encode_token({user_id: user.id}), portafolio: user.portafolio}
     else 
       render json: {error: "Invalid username or password"}, status: :not_acceptable
     end
