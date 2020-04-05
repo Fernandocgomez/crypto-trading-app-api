79804e5d09ec4e385f645e8c21b69d6b4c305a2a
fernandocgomez <fernandocgomez@live.com>
2020-04-05 18:43:00 -0500

    reset db

--- db/migrate/20200404203408_create_balance_trackings.rb
+++ db/migrate/20200404203408_create_balance_trackings.rb
@@ -1,7 +1,7 @@
 class CreateBalanceTrackings < ActiveRecord::Migration[6.0]
   def change
     create_table :balance_trackings do |t|
-      t.float :total
+      t.string :total
       t.integer :portafolio_id
       t.string :date_time
       t.timestamps
